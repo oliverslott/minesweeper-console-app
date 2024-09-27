361d19491490d90eb8dc00dc3f2982f94e53c44d
oliverslott <oliverslottsony@gmail.com>
2024-09-27 17:53:53 +0200

    arrow keys controls

--- Program.cs
+++ Program.cs
@@ -1,66 +1,123 @@
 ﻿//Made by Oliver
 
-const int tileSize = 8;
-const int amountOfBombs = 8;
-int[,] tiles = new int[tileSize, tileSize];
+const byte tileSize = 8;
+const byte amountOfBombs = 8;
+byte[,] tiles = new byte[tileSize, tileSize];
 bool[,] openTiles = new bool[tileSize, tileSize];
+byte selectedX = 0;
+byte selectedY = 0;
 
 SpawnBombs();
-
-while(true)
+Console.WriteLine("Controls: Arrow keys to move cursor and spacebar to open tile");
+while (true)
 {
     DrawBoard();
-    Console.Write("Click tile at(\"x,y\"): ");
-    string[] input = Console.ReadLine().Split(',');
-    int x = int.Parse(input[1]);
-    int y = int.Parse(input[0]);
-    OpenTile(x,y);
+    Controls();
+}
+
+void Controls()
+{
+    //Console.Write("Click tile at(\"x,y\"): ");
+    switch (Console.ReadKey().Key)
+    {
+        case ConsoleKey.LeftArrow:
+            if(selectedY == 0)
+            {
+                selectedY = tileSize - 1;
+            }
+            else
+            {
+                selectedY--;
+            }
+            break;
+        case ConsoleKey.RightArrow:
+            if(selectedY == tileSize - 1)
+            {
+                selectedY = 0;
+            }
+            else
+            {
+                selectedY++;
+            }
+            break;
+        case ConsoleKey.UpArrow:
+            if(selectedX == 0)
+            {
+                selectedX = tileSize - 1;
+            }
+            else
+            {
+                selectedX--;
+            }
+            break;
+        case ConsoleKey.DownArrow:
+            if(selectedX == tileSize - 1)
+            {
+                selectedX = 0;
+            }
+            else
+            {
+                selectedX++;
+            }
+            break;
+        case ConsoleKey.Spacebar:
+            OpenTile(selectedX, selectedY);
+            break;
+    }
+    //string[] input = Console.ReadLine().Split(',');
+    //int x = int.Parse(input[1]);
+    //int y = int.Parse(input[0]);
+    //OpenTile(x,y);
 }
 
 void DrawBoard()
 {
-    for(int x = 0; x < tiles.GetLength(0)+1; x++)
-    { 
-        for(int y = 0; y < tiles.GetLength(1)+1; y++)
+    Console.WriteLine();
+    for (int x = 0; x < tiles.GetLength(0); x++)
+    {
+        for (int y = 0; y < tiles.GetLength(1); y++)
         {
-            if(y == 0 && x == 0)
+            //Cursor
+            if(selectedX == x && selectedY == y)
             {
-                Console.Write("  ");
-                continue;
+                Console.Write("[");
             }
-            if(y == 0)
+            else
             {
-                Console.Write($"{x-1} ");
-                continue;
+                Console.Write(" ");
             }
-            if(x == 0)
+            if (openTiles[x, y])
             {
-                Console.Write($" {y-1}  ");
-                continue;
-            }
-            //Console.Write($"{tiles[x-1,y-1]} ");
-            if(openTiles[x-1,y-1])
-            {
-                if(tiles[x-1,y-1] == 1)
+                if (tiles[x, y] == 1)
                 {
-                    Console.Write($"[X] ");
+                    Console.Write($"X");
                 }
                 else
                 {
-                    int num = GetNumberForTile(x-1,y-1);
-                    if(num == 0)
+                    int num = GetNumberForTile(x, y);
+                    if (num == 0)
                     {
-                        Console.Write($"[□] ");
+                        Console.Write($"□");
                     }
                     else
                     {
-                        Console.Write($"[{num}] ");
+                        Console.Write($"{num}");
                     }
                 }
             }
             else
             {
-                Console.Write($"[■] ");
+                Console.Write($"■");
+            }
+
+            //Cursor
+            if(selectedX == x && selectedY == y)
+            {
+                Console.Write("]");
+            }
+            else
+            {
+                Console.Write(" ");
             }
         }
         Console.WriteLine();
@@ -69,20 +126,20 @@ void DrawBoard()
 
 void OpenTile(int x, int y)
 {
-    if(tiles[x,y] == 1)
+    if (tiles[x, y] == 1)
     {
         Console.WriteLine("You hit a bomb. You lose!");
         //return;
     }
     openTiles[x, y] = true;
-    if(GetNumberForTile(x,y) > 0)
+    if (GetNumberForTile(x, y) > 0)
     {
         return;
     }
     //Below
-    if(y+1<tileSize && tiles[x,y+1] != 1 && !openTiles[x,y+1])
+    if (y + 1 < tileSize && tiles[x, y + 1] != 1 && !openTiles[x, y + 1])
     {
-        OpenTile(x,y+1);
+        OpenTile(x, y + 1);
     }
     // //Below-right
     // if(x+1<tileSize && y+1<tileSize && tiles[x+1,y+1] != 1 && !openTiles[x+1,y+1])
@@ -90,9 +147,9 @@ void OpenTile(int x, int y)
     //     OpenTile(x+1,y+1);
     // }
     //Right
-    if(x+1<tileSize && tiles[x+1,y] != 1 && !openTiles[x+1,y])
+    if (x + 1 < tileSize && tiles[x + 1, y] != 1 && !openTiles[x + 1, y])
     {
-        OpenTile(x+1,y);
+        OpenTile(x + 1, y);
     }
     // //Top-right
     // if(y-1 >= 0 && x+1<tileSize && tiles[x+1,y-1] != 1 && !openTiles[x+1,y-1])
@@ -100,9 +157,9 @@ void OpenTile(int x, int y)
     //     OpenTile(x+1,y-1);
     // }
     //Top
-    if(y-1 >= 0 && tiles[x,y-1] != 1 && !openTiles[x,y-1])
+    if (y - 1 >= 0 && tiles[x, y - 1] != 1 && !openTiles[x, y - 1])
     {
-        OpenTile(x,y-1);
+        OpenTile(x, y - 1);
     }
     // //Top-left
     // if(y-1 >= 0 && x-1 >= 0 && tiles[x-1,y-1] != 1 && !openTiles[x-1,y-1])
@@ -110,9 +167,9 @@ void OpenTile(int x, int y)
     //     OpenTile(x-1,y-1);
     // }
     //Left 
-    if(x-1 >= 0 && tiles[x-1,y] != 1 && !openTiles[x-1,y])
+    if (x - 1 >= 0 && tiles[x - 1, y] != 1 && !openTiles[x - 1, y])
     {
-        OpenTile(x-1,y);
+        OpenTile(x - 1, y);
     }
     // //Below-left
     // if(x-1 >= 0 && y+1<tileSize && tiles[x-1,y+1] != 1 && !openTiles[x-1,y+1])
@@ -127,92 +184,52 @@ int GetNumberForTile(int x, int y)
     int bombsAroundTile = 0;
 
     //Below
-    if(y+1<tileSize && tiles[x,y+1] == 1)
+    if (y + 1 < tileSize && tiles[x, y + 1] == 1)
     {
         bombsAroundTile++;
     }
     //Below-right
-    if(x+1<tileSize && y+1<tileSize && tiles[x+1,y+1] == 1)
+    if (x + 1 < tileSize && y + 1 < tileSize && tiles[x + 1, y + 1] == 1)
     {
         bombsAroundTile++;
     }
     //Right
-    if(x+1<tileSize && tiles[x+1,y] == 1)
+    if (x + 1 < tileSize && tiles[x + 1, y] == 1)
     {
         bombsAroundTile++;
     }
     //Top-right
-    if(y-1 >= 0 && x+1<tileSize && tiles[x+1,y-1] == 1)
+    if (y - 1 >= 0 && x + 1 < tileSize && tiles[x + 1, y - 1] == 1)
     {
         bombsAroundTile++;
     }
     //Top
-    if(y-1 >= 0 && tiles[x,y-1] == 1)
+    if (y - 1 >= 0 && tiles[x, y - 1] == 1)
     {
         bombsAroundTile++;
     }
     //Top-left
-    if(y-1 >= 0 && x-1 >= 0 && tiles[x-1,y-1] == 1)
+    if (y - 1 >= 0 && x - 1 >= 0 && tiles[x - 1, y - 1] == 1)
     {
         bombsAroundTile++;
     }
     //Left 
-    if(x-1 >= 0 && tiles[x-1,y] == 1)
+    if (x - 1 >= 0 && tiles[x - 1, y] == 1)
     {
         bombsAroundTile++;
     }
     //Below-left
-    if(x-1 >= 0 && y+1<tileSize && tiles[x-1,y+1] == 1)
+    if (x - 1 >= 0 && y + 1 < tileSize && tiles[x - 1, y + 1] == 1)
     {
         bombsAroundTile++;
     }
     return bombsAroundTile;
 }
 
-// bool Below()
-// {
-//     return 
-// }
-
-// bool BelowRight()
-// {
-
-// }
-
-// bool Right()
-// {
-
-// }
-
-// bool TopRight()
-// {
-
-// }
-
-// bool Top()
-// {
-
-// }
-
-// bool TopLeft()
-// {
-
-// }
-
-// bool Left()
-// {
-
-// }
-
-// bool BelowLeft()
-// {
-
-// }
-
 void SpawnBombs()
 {
     Random rand = new Random();
-    for(int i = 0; i < amountOfBombs; i++)
+    for (int i = 0; i < amountOfBombs; i++)
     {
         int randX = rand.Next(0, tileSize);
         int randY = rand.Next(0, tileSize);
@@ -220,9 +237,3 @@ void SpawnBombs()
     }
     //int randX = r
 }
-
-// enum Tiles
-// {
-//     NotClicked,
-//     Clicked,
-// }
